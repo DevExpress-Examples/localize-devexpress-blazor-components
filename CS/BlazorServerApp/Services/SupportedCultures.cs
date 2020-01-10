c89a513984886be54d5988019befe30f05687754
ovcharov.pavel <1357165+pavelovcharov@users.noreply.github.com>
2020-01-10 18:04:09 +0300

    add example content

---
+++ CS/BlazorServerApp/Services/SupportedCultures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Services {
+    public class SupportedCultures {
+        public static CultureInfo[] Cultures { get; } = new CultureInfo[] {
+            new CultureInfo("de"),
+            new CultureInfo("en"),
+            new CultureInfo("es"),
+            new CultureInfo("ja"),
+            new CultureInfo("ru"),
+        };
+    }
+}
