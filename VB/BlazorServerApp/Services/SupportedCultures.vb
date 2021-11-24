d7ea50d69efc85403e63fe790b7752cc32f576d2
DevExpressExampleBot <fakeEmail>
2021-11-24 14:40:36 +0300

    autogenerate VB

--- VB/BlazorServerApp/Services/SupportedCultures.vb
+++ VB/BlazorServerApp/Services/SupportedCultures.vb
@@ -1,15 +1,9 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Globalization
-Imports System.Linq
-Imports System.Threading.Tasks
 
 Namespace BlazorServerApp.Services
-	Public Class SupportedCultures
-		Public Shared ReadOnly Property Cultures() As CultureInfo() = {
-			New CultureInfo("de-DE"),
-			New CultureInfo("en-US"),
-			New CultureInfo("ru-RU")
-		}
-	End Class
+
+    Public Class SupportedCultures
+
+        Public Shared ReadOnly Property Cultures As CultureInfo() = New CultureInfo() {New CultureInfo("de-DE"), New CultureInfo("en-US"), New CultureInfo("ru-RU")}
+    End Class
 End Namespace
