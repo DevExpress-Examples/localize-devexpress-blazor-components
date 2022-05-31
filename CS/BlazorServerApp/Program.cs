2124628d8ea1a45fe096a8de1b338d66f99bb7c1
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2022-05-31 12:35:51 +0400

    22.1 update server app

--- CS/BlazorServerApp/Program.cs
+++ CS/BlazorServerApp/Program.cs
@@ -1,24 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+using BlazorServerApp.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Linq;
 
-namespace BlazorServerApp {
+namespace BlazorServerApp
+{
     public class Program {
         public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddControllers();
+            builder.Services.AddDevExpressBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            var supportedCultures = SupportedCultures.Cultures.Select(x => x.Name).ToArray();
+            var localizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+            app.Run();
+        }
     }
 }
